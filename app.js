5c34311786b7c3333c17fe9b5ce3520de0c526a7
Michael <100490812+DebSol11@users.noreply.github.com>
2026-03-30 00:42:50 +0200

    No no no

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-// const path = require("node:path");
+const path = require("node:path");
 
 app.get("/", (req, res) => {
   res.send("🚀 Server running perfectly!");
 });
 
-// app.get("/", (req, res) => {
-//   res.render("index", { message: "EJS rocks!" });
-// });
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
 
 app.post("/api/contact", (req, res) => {
   console.log("📩 Message received:", req.body);
@@ -19,5 +19,5 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
 // Views Chapter
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
